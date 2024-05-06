7d87ae4804183fb9f48c798d6b9ecdf83b9182f3
marystuartk <164695576+marystuartk@users.noreply.github.com>
2024-05-06 20:10:43 +0000

    save

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,4 +5,40 @@ matching_photos = Photo.all
 
     render({ :template => "photo_templates/index" })
   end
+
+  def show
+  url_id = params.fetch("path_id")
+  matching_photos = Photo.where({ :id => url_id })
+  @the_photo = matching_photos.at(0)
+
+    render({ :template => "photo_templates/show" })
+  end
+  
+  def baii
+  the_id = params.fetch("toast_id")
+  matching_photos = Photo.where({ :id => the_id })
+  the_photo = matching_photos.at(0)
+  the_photo.destroy
+  redirect_to("/photos")
+
+    # render({ :template => "photo_templates/baii" })
+  end
+
+  def create
+  input_image = params.fetch("query_image")
+  input_caption = params.fetch("query_caption")
+  input_owner_id = params.fetch("query_owner_id")
+
+  a_new_photo = Photo.new
+
+  a_new_photo.image = input_image
+  a_new_photo.caption = input_caption
+  a_new_photo.owner_id = input_owner_id
+
+
+  a_new_photo.save
+
+    render({ :template => "photo_templates/create" })
+  end
+
 end
